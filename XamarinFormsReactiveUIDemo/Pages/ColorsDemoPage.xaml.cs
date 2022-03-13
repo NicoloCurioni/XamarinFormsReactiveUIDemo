07c6271ed496d949a9ca025ad87017c7037a764a
Nicolò Curioni <nicolo.curioni@yahoo.it>
2022-03-13 11:12:42 +0100

    Added Observables to observe and change Background color dynamically using sliders

---
+++ XamarinFormsReactiveUIDemo/Pages/ColorsDemoPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamarinFormsReactiveUIDemo.Pages
+{
+    public partial class ColorsDemoPage : ContentPage
+    {
+        public ColorsDemoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
