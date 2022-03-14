57092fef4cb066e0b175d93f6abeb1cb724fb2c8
Nicolò <nicolo.curioni@consulenti.hype.it>
2022-03-14 10:09:49 +0100

    Added a ListView with a list of mocked Contacts data, allowing the user to filter, during the search.

--- XamarinFormsReactiveUIDemo/App.xaml.cs
+++ XamarinFormsReactiveUIDemo/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XamarinFormsReactiveUIDemo.Pages;
 
 namespace XamarinFormsReactiveUIDemo
 {
@@ -10,7 +11,7 @@ namespace XamarinFormsReactiveUIDemo
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new ContactsPage();
         }
 
         protected override void OnStart()
---
+++ XamarinFormsReactiveUIDemo/Pages/ContactsPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using XamarinFormsReactiveUIDemo.ViewModels;
+
+namespace XamarinFormsReactiveUIDemo.Pages
+{
+    public partial class ContactsPage : ContentPage
+    {
+        public ContactsPage()
+        {
+            InitializeComponent();
+            BindingContext = new ContactsViewModel();
+        }
+    }
+}
